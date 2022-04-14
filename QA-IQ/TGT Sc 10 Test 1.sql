fe9af9a58bc8130bac030b55a4d129f9a0162e00
Aahmed30 <89610760+Aahmed30@users.noreply.github.com>
2022-04-14 11:27:10 -0500

    Delete QA-IQ directory
    Updating/organizing and reloading.

--- QA-IQ/TGT Sc 10 Test 1.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    (SELECT COUNT(DISTINCT invoice_Id) FROM Events_Payments 
-     WHERE invoice_Id IS NOT NULL AND CAST(payment_amount AS DOUBLE) <> 0) AS pCountExpected
-     --13,006,486
-     
-     
-   SELECT COUNT(DISTINCT Transaction_ID) AS tgtCountActual FROM Tender_Group_Type
-   --12,960,863
